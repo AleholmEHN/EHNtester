252fe47f1ee59bf3620c5536ed6d85ad2a1f2054
Elin Henriksson <elin.henriksson@savsjo.se>
2014-09-30 12:20:36 +0200

    katt

--- mittscript.js
+++ mittscript.js
@@ -2,6 +2,10 @@
 
 function start()
 {
-    line(0,innerHeight,innerWidth, innerHeight, 10, "black");
+    line(0,totalHeight,totalWidth, totalHeight, 10, "black");
+    
+    line(0, totalHeight/2, totalWidth, totalHeight/2, 5, "red");
+    
+    line(totalWidth/2, 0, totalWidth/2, totalHeight, 4, "yellow");
     
 }
